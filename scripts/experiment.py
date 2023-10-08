d18f4aee83ecd1a167e6e6bf21cd553b5127af0d
cybaj <cybaj@naver.com>
2023-10-09 08:50:48 +0900

    change log dir decide

--- scripts/experiment.py
+++ scripts/experiment.py
@@ -12,9 +12,10 @@ parser.add_argument('--name', type=str, default='experiment')
 trpcts = [50, 25, 10]
 batchfracs = [0.5, 0.4, 0.3, 0.2]
 operators = grok.data.VALID_OPERATORS.keys()
+operators_dict = grok.data.VALID_OPERATORS
 
 def format_command(batchfrac, trpct, operator, args):
-    logdir = f'{args.name}/run-{operator}-batchfrac-{batchfrac}-trpct-{trpct}'
+    logdir = f'{args.name}/run-{operators_dict[operator]}-batchfrac-{batchfrac}-trpct-{trpct}'
     return f'python scripts/train.py --logdir={logdir} --gpu={args.gpu} --batchsize={batchfrac} --train_data_pct={trpct} --math_operator={operator} --weight_decay={args.weight_decay} --max_steps={args.max_steps}'
 
 def plan(args):
