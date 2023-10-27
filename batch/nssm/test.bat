d8967f4d3c0a02f248b3e82f4aedb019e87580db
cybaj <cybaj@naver.com>
2023-10-28 07:55:39 +0900

    feat: add nssm batch

---
+++ batch/nssm/test.bat
@@ -0,0 +1,3 @@
+@echo off
+call C:\Users\tomat\ws\gong\openai-grok\.venv\Scripts\activate
+python C:\Users\tomat\ws\gong\openai-grok\scripts\experiment.py --name expt-test-1 --gpu 0 --duplication 0.5
